7879f5ae8ddddcd3253fdd76030a9cf287ada5a7
faruq <faruqmansuri707@gmail.com>
2023-12-22 08:51:53 +0530

    boilerplate for all app

---
+++ components/MemberProfile.jsx
@@ -0,0 +1,17 @@
+import { auth, currentUser, UserButton } from '@clerk/nextjs';
+
+const MemberProfile = async () => {
+  const user = await currentUser();
+  // const { userId } = auth();
+
+  // console.log(user);
+  // console.log(userId);
+  return (
+    <div className='px-4 flex items-center gap-2'>
+      <UserButton afterSignOutUrl='/' />
+      <p>{user.emailAddresses[0].emailAddress}</p>
+    </div>
+  );
+};
+
+export default MemberProfile;
---
+++ components/ThemeToggle.jsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useState } from 'react';
+import { BsMoonFill, BsSunFill } from 'react-icons/bs';
+
+const themes = {
+  winter: 'winter',
+  night: 'night',
+};
+const ThemeToggle = () => {
+  const [theme, setTheme] = useState(themes.winter);
+
+  const toggleTheme = () => {
+    const newTheme = theme === themes.winter ? themes.night : themes.winter;
+    document.documentElement.setAttribute('data-theme', newTheme);
+    setTheme(newTheme);
+  };
+
+  return (
+    <button className='btn btn-sm btn-outline ' onClick={toggleTheme}>
+      {theme === themes.winter ? (
+        <BsMoonFill className='h-4 w-4 ' />
+      ) : (
+        <BsSunFill className='h-4 w-4 ' />
+      )}
+    </button>
+  );
+};
+
+export default ThemeToggle;
